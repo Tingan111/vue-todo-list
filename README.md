17b48d3ef6201addd05ce836397e103fb5c456f7
Tingan <syeak123@gmail.com>
2025-02-16 17:55:08 +0800

    fear:todo-list

---
+++ README.md
@@ -0,0 +1,5 @@
+# vue-todo-list
+啟動 npm run dev
+
+代辦事項
+新增刪除# vue-todo-list
---
+++ src/todo-list.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from "vue"
+let id=0
+const newTodo=ref('')
+const Todos=ref([
+  {id:id++,content:"第一名"},
+  {id:id++,content:"第二名"},
+  {id:id++,content:"第三名"},
+])
+function addTodo(){
+  Todos.value.push({id:id++,content:newTodo.value})
+  newTodo.value=""
+}
+function removeTodo(todo){
+Todos.value=Todos.value.filter((t)=>t!=todo)
+
+}
+
+</script>
+<template><form @submit.prevent="addTodo">
+  <input v-model="newTodo" required placeholder="new todo">
+  <button>新增</button>
+  <ul>
+    <li v-for="todo in Todos" :key="todo.id">{{ todo.id }},{{ todo.content }}
+      <button @click="removeTodo(todo)">X</button>
+
+    </li>
+  </ul>
+   
+</form>
+</template>
